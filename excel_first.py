51d45e7df050a510113378a95f48419b02d39814
milk0428 <112978461@qq.com>
2018-07-03 09:48:07 +0800

    expand()

--- excel_first.py
+++ excel_first.py
@@ -1,7 +1,7 @@
 import xlwings as xw
 
 workbook=xw.Book(r'国开债价格监测表0705.xls')
-date_range=workbook.sheets("国开债价格监测表（连续日期）").range(1,1)
-print(date_range.column)
+date_range=workbook.sheets("国开债价格监测表（连续日期）").range(12,1).expand()
+print(date_range)
 
 workbook.close()

111e89a9d587e11868b48a7af68966248bbc623e
milk0428 <112978461@qq.com>
2018-07-07 22:40:42 +0800

    expand()

--- excel_first.py
+++ excel_first.py
@@ -1,5 +1,5 @@
 import xlwings as xw
-
+import datetime
 workbook=xw.Book(r'国开债价格监测表0705.xls')
 thisSheet=workbook.sheets("国开债价格监测表（连续日期）")
 # date_range=workbook.sheets("国开债价格监测表（连续日期）").range('a3:D16').value
@@ -12,6 +12,15 @@ row=thisSheet['c3'].end('down').row
 #最后一列列标
 column=thisSheet['c3'].end('right').column
 print(row,column)
+#
+# # datetime.timedelta.__divmod__(36000)
+#
+# #计算两个日期之间相差天数
+# days=thisSheet["C5"].value.date()-thisSheet["C4"].value.date()
+# print(int(days.total_seconds()/86400))
+# print(thisSheet.range((1,1),(row,column)).value)
 
+new_worksheet=workbook.sheets.add()
+new_worksheet.range(1,1).value=thisSheet.range((1,1),(row,column)).value
 
-workbook.close()
+# workbook.close()

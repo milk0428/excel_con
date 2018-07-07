212dcec4d31cc8c51dbc13926842ef2505c2dda0
milk0428-latop <112978461@qq.com>
2018-07-08 00:23:08 +0800

    获取到行列边界

--- excel_first.py
+++ excel_first.py
@@ -1,7 +1,17 @@
 import xlwings as xw
 
 workbook=xw.Book(r'国开债价格监测表0705.xls')
-date_range=workbook.sheets("国开债价格监测表（连续日期）").range(1,1)
-print(date_range.column)
+thisSheet=workbook.sheets("国开债价格监测表（连续日期）")
+# date_range=workbook.sheets("国开债价格监测表（连续日期）").range('a3:D16').value
+# print(date_range)
+
+#获取整个表格数据
+
+#最后一行行标
+row=thisSheet['c3'].end('down').row
+#最后一列列标
+column=thisSheet['c3'].end('right').column
+print(row,column)
+
 
 workbook.close()
